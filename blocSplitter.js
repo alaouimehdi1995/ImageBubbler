f3935c6afcf50b07cd102968e8f85a82519ee3e3
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-08-11 17:44:49 +0100

    First commit

---
+++ blocSplitter.js
@@ -0,0 +1,71 @@
+/**
+ * Created by mehdi on 11/08/17.
+ */
+
+
+function split(imageCanvas,e){
+    var imageContext=imageCanvas.getContext("2d");
+    console.log("split called");
+    var element=e.target;
+    var x=element.left;
+    var y=element.top;
+    var parent=element.parentNode;
+    parent.removeChild(element);
+    var c1,c2,c3,c4;
+    var marginProportion=0.2;
+    c1=document.createElement("div");
+    c1.className='bloc';
+    c1.style.width=(parseFloat(element.style.width)/2)+"px";
+    c1.style.height=c1.style.width;
+    c1.style.top=element.style.top;
+    c1.style.left=element.style.left;
+    //c1.style.margin=marginProportion*parseFloat(c1.style.width)+"px";
+
+    //console.log("c1 position:\n"+parseFloat(c1.style.left)+","+parseFloat(c1.style.left)+parseFloat(c1.style.width)+","+parseFloat(c1.style.top)+","+parseFloat(c1.style.top)+parseFloat(c1.style.height));
+    //console.log("element middle:\n"+parseFloat(element.style.left)+","+parseFloat(element.style.left)+parseFloat(c1.style.width)+","+parseFloat(element.style.top)+","+parseFloat(c1.style.top)+parseFloat(c1.style.height));
+
+
+
+    var pixl=getAverageColor(imageContext,parseFloat(c1.style.left),parseFloat(c1.style.left)+parseFloat(c1.style.width),parseFloat(c1.style.top),parseFloat(c1.style.top)+parseFloat(c1.style.height),35);
+    c1.style.backgroundColor = "rgb(" + pixl.R + "," + pixl.G + "," + pixl.B + ")";
+    c1.addEventListener("mousemove",function(e){split(imageCanvas,e);},false);
+
+    c2=document.createElement("div");
+    c2.className='bloc';
+    c2.style.width=(parseFloat(element.style.width)/2)+"px";
+    c2.style.height=c1.style.width;
+    c2.style.top=element.style.top;
+    //c2.style.margin=marginProportion*parseFloat(c1.style.width)+"px";
+    c2.style.left=(parseFloat(element.style.left)+parseFloat(c1.style.width))+"px";
+    pixl=getAverageColor(imageContext,parseFloat(c2.style.left),parseFloat(c2.style.left)+parseFloat(c2.style.width),parseFloat(c2.style.top),parseFloat(c2.style.top)+parseFloat(c2.style.height),35);
+    c2.style.backgroundColor = "rgb(" + pixl.R + "," + pixl.G + "," + pixl.B + ")";
+    c2.addEventListener("mousemove",function(e){split(imageCanvas,e);},false);
+
+    c3=document.createElement("div");
+    c3.className='bloc';
+    c3.style.width=(parseFloat(element.style.width)/2)+"px";
+    c3.style.height=c1.style.width;
+    c3.style.left=element.style.left;
+    //c3.style.margin=marginProportion*parseFloat(c1.style.width)+"px";
+    c3.style.top=(parseFloat(element.style.top)+parseFloat(c1.style.height))+"px";
+    pixl=getAverageColor(imageContext,parseFloat(c3.style.left),parseFloat(c3.style.left)+parseFloat(c3.style.width),parseFloat(c3.style.top),parseFloat(c3.style.top)+parseFloat(c3.style.height),35);
+    c3.style.backgroundColor = "rgb(" + pixl.R + "," + pixl.G + "," + pixl.B + ")";
+    c3.addEventListener("mousemove",function(e){split(imageCanvas,e);},false);
+
+    c4=document.createElement("div");
+    c4.className='bloc';
+    c4.style.width=(parseFloat(element.style.width)/2)+"px";
+    c4.style.height=c1.style.width;
+    //c4.style.margin=marginProportion*parseFloat(c1.style.width)+"px";
+    c4.style.top=(parseFloat(element.style.top)+parseFloat(c2.style.height))+"px";
+    c4.style.left=(parseFloat(element.style.left)+parseFloat(c2.style.width))+"px";
+    pixl=getAverageColor(imageContext,parseFloat(c4.style.left),parseFloat(c4.style.left)+parseFloat(c4.style.width),parseFloat(c4.style.top),parseFloat(c4.style.top)+parseFloat(c4.style.height),35);
+    c4.style.backgroundColor = "rgb(" + pixl.R + "," + pixl.G + "," + pixl.B + ")";
+    c4.addEventListener("mousemove",function(e){split(imageCanvas,e);},false);
+
+    parent.appendChild(c1);
+    parent.appendChild(c2);
+    parent.appendChild(c3);
+    parent.appendChild(c4);
+}
+
---
+++ mainExtractor.js
@@ -0,0 +1,67 @@
+/**
+ * Created by mehdi on 11/08/17.
+ */
+function el(id){return document.getElementById(id);} // Get elem by ID
+
+var resultCanvas  = el("colorMap");
+var context;
+
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+            var img = new Image();
+
+            img.addEventListener("load", function() {
+
+
+                var imageCanvas=document.createElement("canvas");
+                imageCanvas.width=img.width;
+                imageCanvas.height=img.height;
+                var imageContext=imageCanvas.getContext("2d");
+                imageContext.drawImage(img, 0, 0);
+
+                var pix=getAverageColor(imageContext,0,imageCanvas.width,0,imageCanvas.height,35);
+                //Calling displayers
+
+
+                var proportion=0.9; //window proportion
+
+                resultCanvas.style.width=proportion*parseFloat(screen.width)+"px";
+                resultCanvas.style.height=parseFloat(resultCanvas.style.width)*(parseFloat(img.height)/parseFloat(img.width))+"px";
+
+
+                var newDiv=document.createElement('div');
+                newDiv.className='bloc';
+                newDiv.style.width=Math.min(parseFloat(resultCanvas.style.width),parseFloat(resultCanvas.style.height))+"px";
+                newDiv.style.height=newDiv.style.width;
+                newDiv.style.backgroundColor="rgb(" + pix.R + "," + pix.G + "," + pix.B + ")";
+                setMiddle(resultCanvas,newDiv,0,resultCanvas.style.width,0,resultCanvas.style.height);
+                resultCanvas.appendChild(newDiv);
+                newDiv.addEventListener("mousemove",function(e){split(imageCanvas,e);},false);
+
+            });
+            img.src = e.target.result;
+        };
+        FR.readAsDataURL( this.files[0] );
+
+    }
+}
+
+
+
+
+
+el("fileUpload").addEventListener("change", readImage, false);
+
+
+function setMiddle(parent,child,widthStart,widthEnd,heightStart,heightEnd){
+    child.style.left=(parseFloat(widthEnd)+parseFloat(widthStart)-parseFloat(child.style.width))+"px";
+    child.style.top=(parseFloat(heightEnd)+parseFloat(heightStart)-parseFloat(child.style.height))+"px";
+    console.log("blabladone");
+}
+
+
+
+
